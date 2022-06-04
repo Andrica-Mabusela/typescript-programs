bc7dd7e4692e1f9e9566dc9df6bf207bf2a2133e
Andrica-Mabusela <programmerandy420@gmail.com>
2022-06-04 22:18:46 +0200

    type alias

---
+++ public/alias.js
@@ -0,0 +1,2 @@
+"use strict";
+// So whenever we want to use string | number as data type, we can just write stringOrNum instead.
---
+++ src/alias.ts
@@ -0,0 +1,5 @@
+// If we have a specific union type that we use in multiple places, we can create an alias for it.
+// EXAMPLE:-
+type stringOrNum = string | number
+
+// So whenever we want to use string | number as data type, we can just write stringOrNum instead.
